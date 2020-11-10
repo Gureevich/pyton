5bd3ad8958c806d1b99b509e4f30e7a1c6459b3b
gureevich <ya.gurey@gmail.com>
2020-11-10 13:00:17 +1200

    1

--- lesson_1/hw_1_2.py
+++ lesson_1/hw_1_2.py
@@ -2,4 +2,5 @@ a = input ("Vvedite kolichestvo sekund ")
 b = a % 60
 c = a // 3600
 d = a // 60 - c * 60
-print (str(c) + ':' + str(d) + ':' + str(b))
+vremya = f"{c:02}:{d:02}:{b:02}"
+print (vremya)
